b1df890a4f902d2662125d3a8a7ec2eb052fd3d5
PhamDucTHinh <thanhtungph23@gmail.com>
2025-03-23 13:09:19 +0700

    books

---
+++ Models/books.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+from pydantic import BaseModel
+
+
+class Books(Base):
+    __tablename__ = "Books"
+
+    book_id = Column(String(255), primary_key=True, nullable=False)
+    name = Column(String(255), nullable=False)
+    quantily = Column(int, nullable=True)
+    price = Column(int, nullable=True)
+    published_year = Column(String(255), nullable=True)
+    publisher_id = Column(String(255), nullable=True)
+    author_id = Column(String(50), nullable=True)
+
+class BooksSchema(BaseModel):
+    book_id: str
+    name: str
+    quantily: int | None = None
+    price: int | None = None
+    published_year: str | None = None
+    publisher_id: str | None = None
+    author_id: str | None = None
+
+    class Config:
+        from_attributes = True
